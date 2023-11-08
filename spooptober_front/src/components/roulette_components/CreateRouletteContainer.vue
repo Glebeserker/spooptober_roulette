55c8cbee75fdc7a985cc22a81facd37cd7d54765
Glebeserker <belyagle001@gmail.com>
2023-11-08 16:18:35 -0600

    small add

--- spooptober_front/src/components/roulette_components/CreateRouletteContainer.vue
+++ spooptober_front/src/components/roulette_components/CreateRouletteContainer.vue
@@ -3,9 +3,22 @@
 </script>
 
 <template>
-
+    <div class="rouletteCreateBoxWrap">
+        <div class="columns">
+            <div class="column box roulButton">
+                <p class="has-text-centered">
+                    New <span>+</span>
+                </p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped lang="scss">
 
+
+.roulButton{
+    padding: 2rem;
+    cursor: pointer;
+}
 </style>
